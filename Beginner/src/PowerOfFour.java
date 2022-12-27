5321b150feee1a02555e26155810294557b47652
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2022-12-27 19:06:16 +0530

    Interview Questions

---
+++ Beginner/src/PowerOfFour.java
@@ -0,0 +1,9 @@
+
+public class PowerOfFour {
+    public boolean isPowerOfFour(int n) {
+        if(n<1)return false;
+        if(n==1)return true;
+        return (n & (n-1)) == 0 && ((Integer.toBinaryString(n).length()-1) %2==0 );
+
+    }
+}
