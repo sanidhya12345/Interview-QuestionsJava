a03e43393bf5cd70044a59fbe9e2f4a19f7d9937
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2022-12-27 19:06:41 +0530

    Interview Questions

---
+++ Beginner/src/QueuewithMinimum.java
@@ -0,0 +1,32 @@
+
+
+import java.io.PrintWriter;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueuewithMinimum {
+    PrintWriter out;
+    Queue<Long> queue = new LinkedList<>();
+    LinkedList<Long> mins = new LinkedList<>();
+
+    private void pop() {
+        long last = queue.poll();
+        if (mins.peek().compareTo(last) == 0) mins.poll();
+    }
+
+    private void min() {
+        out.println(mins.peek());
+    }
+
+    private void push(long i) {
+        queue.add(i);
+        if (mins.isEmpty() || mins.peekLast().compareTo(i) <= 0)
+            mins.add(i);
+        else {
+            while (!mins.isEmpty() && mins.peekLast().compareTo(i) > 0) {
+                mins.removeLast();
+            }
+            mins.add(i);
+        }
+    }
+}
