f860383d261bca7ee08818e306c4e6f8a5b06fe6
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2022-12-27 19:05:11 +0530

    Interview Questions

---
+++ Beginner/src/PascalsTriangle.java
@@ -0,0 +1,23 @@
+
+public class PascalsTriangle {
+    public List<List<Integer>> generate(int n) {
+        List<List<Integer>> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            List<Integer> l = new ArrayList<>(i);
+            if (i == 0) {
+                l.add(1);
+                list.add(l);
+                continue;
+            }
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {
+                    l.add(1);
+                } else {
+                    l.add(list.get(i - 1).get(j) + list.get(i - 1).get(j - 1));
+                }
+            }
+            list.add(l);
+        }
+        return list;
+    }
+}
