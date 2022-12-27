83eef676083f4c22cb191f8eb3b79dc33e0af7a3
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2022-12-27 19:08:28 +0530

    Interview Questions

---
+++ Beginner/src/RemoveZeroes.java
@@ -0,0 +1,12 @@
+
+public class RemoveZeroes {
+    public void moveZeroes(int[] a) {
+        int j=0;
+        for(int i=0; i<a.length; i++)
+            if(a[i]!=0)a[j++]=a[i];
+
+        for(int i=j; i<a.length; i++)
+            a[i]=0;
+
+    }
+}
