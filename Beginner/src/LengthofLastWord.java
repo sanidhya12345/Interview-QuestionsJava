93fe455d53f08cbc8e57c3fd6415d44a85c5ded5
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2022-12-27 19:02:18 +0530

    Interview Questions

---
+++ Beginner/src/LengthofLastWord.java
@@ -0,0 +1,11 @@
+
+public class LengthofLastWord {
+    public int lengthOfLastWord(String s) {
+        if (s == null || s.length() == 0) return 0;
+        s = s.trim();
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == ' ') return s.length() - i - 1;
+        }
+        return s.length();
+    }
+}
