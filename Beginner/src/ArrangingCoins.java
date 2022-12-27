78e13016b1ef5d97c77cb59472d372007e7ee68c
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2022-12-27 17:45:50 +0530

    interview questions

---
+++ Beginner/src/ArrangingCoins.java
@@ -0,0 +1,11 @@
+
+public class ArrangingCoins {
+    public static void main(String[] args) {
+        System.out.println(1<<3);
+    }
+
+    public int arrangeCoins(int n) {
+        long count= (-1 + ((long)Math.sqrt(1+4*2.0*n)))/2;
+        return Long.valueOf(count).intValue();
+    }
+}
