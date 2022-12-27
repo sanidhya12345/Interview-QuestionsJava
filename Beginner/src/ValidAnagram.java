ddf2b3ad6ace9480bc396e2bd097e41312d723b4
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2022-12-27 19:14:27 +0530

    Interview Questions

---
+++ Beginner/src/ValidAnagram.java
@@ -0,0 +1,14 @@
+
+public class ValidAnagram {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+        int[] alphabet = new int[26];
+        for (int i = 0; i < s.length(); i++) alphabet[s.charAt(i) - 'a']++;
+        for (int i = 0; i < t.length(); i++) {
+            int index = t.charAt(i) - 'a';
+            if (alphabet[index] == 0) return false;
+            alphabet[index]--;
+        }
+        return true;
+    }
+}
