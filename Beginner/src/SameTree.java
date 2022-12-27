a4c5298532e1a1ea212c8826b8d32a81398eac8a
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2022-12-27 19:10:24 +0530

    Interview Questions

---
+++ Beginner/src/SameTree.java
@@ -0,0 +1,13 @@
+package interviewquestions.easy;
+
+import interviewquestions.utils.TreeNode;
+
+/**
+ * Created by sherxon on 2016-12-24.
+ */
+public class SameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null && q==null)return true;
+        return p!=null && q!=null && isSameTree(p.left, q.left) && isSameTree(p.right, q.right) && q.val==p.val;
+    }
+}
