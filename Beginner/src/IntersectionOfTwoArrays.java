8129a792cb44d66d1405ab8c803b32e2e1db3cfc
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2022-12-27 19:00:34 +0530

    Interview Questions

---
+++ Beginner/src/IntersectionOfTwoArrays.java
@@ -0,0 +1,24 @@
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+public class IntersectionOfTwoArrays {
+    public int[] intersection(int[] nums1, int[] nums2) {
+
+        Set<Integer> set=new HashSet<>();
+
+        for(int i:nums1)
+            if(!set.contains(i))set.add(i);
+        Set<Integer> set2=new HashSet<>();
+        for(int i:nums2)
+            if(set.contains(i))set2.add(i);
+        int[] a= new int[set2.size()];
+        int i=0;
+        for(Integer inte: set2){
+            a[i++]=inte;
+        }
+        return a;
+
+    }
+}
