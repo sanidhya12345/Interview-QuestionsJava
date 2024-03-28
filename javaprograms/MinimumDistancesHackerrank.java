43aa8fdc0b785a105e88faa05a3d8a9d247184e5
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2024-03-28 18:26:33 +0530

    Create MinimumDistancesHackerrank.java

---
+++ javaprograms/MinimumDistancesHackerrank.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class MinimumDistancesHackerrank {
+
+    /*
+     * Complete the 'minimumDistances' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static int minimumDistances(List<Integer> a) {
+    int minDist = Integer.MAX_VALUE;
+
+    for (int i = 0; i < a.size(); i++) {   
+        for (int j = i + 1; j < a.size(); j++) {
+            if (a.get(i).equals(a.get(j))) {
+                minDist = Math.min(minDist, Math.abs(i - j));
+            }
+        }
+    }
+    return (minDist == Integer.MAX_VALUE) ? -1 : minDist;
+}
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.minimumDistances(a);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
