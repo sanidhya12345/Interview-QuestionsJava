5a0444faed92089063121ea1bc2bf544cf5d4f12
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2024-03-30 17:09:04 +0530

    Update MetTheirTargetEasy.java

--- javaprograms/MetTheirTargetEasy.java
+++ javaprograms/MetTheirTargetEasy.java
@@ -1,24 +1,11 @@
 public class MetTheirTargetEasy {
-    public int[] rowAndMaximumOnes(int[][] mat) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int i=0;i<mat.length;i++){
-            int countOne=0;
-            for(int j=0;j<mat[0].length;j++){
-                if(mat[i][j]==1){
-                    countOne++;
-                }
-            }
-            map.put(i,countOne);
-        }
-        int [] ans=new int[2];
-        int maxValue=Collections.max(map.values());
-        for(Map.Entry<Integer,Integer> entry:map.entrySet()){
-            if(entry.getValue()==maxValue){
-                ans[0]=entry.getKey();
-                ans[1]=maxValue;
-                break;
+    public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+        int count=0;
+        for(Integer i:hours){
+            if(i>=target){
+                count++;
             }
         }
-        return ans;
+        return count;
     }
 }
